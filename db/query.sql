b4561df928130d4b0e2ff9185c7a7e2387e06aa4
Timothy Wardlow <wardlowtimothy@icloud.com>
2022-11-22 20:21:02 -0600

    removing sample query file

--- db/query.sql
+++
@@ -1,2 +0,0 @@
-SELECT employee_id, name, role_id, title, salary, first_name, last_name, manager_id
-FROM ((departments JOIN roles ON departments.id = roles.department_id) JOIN employees ON roles.id = employees.role_id);
